c282d68477d852c60a5dca94ef55945d35d99577
StrayBird_ATSH <zhanghuilian-98022@163.com>
2018-10-31 00:41:28 +0800

    The split child method is not working well.

--- src/BPlusTreeWC.java
+++ src/BPlusTreeWC.java
@@ -47,35 +47,35 @@ public class BPlusTreeWC extends StringTree {
 
     private void BTreeSplitChild(@NotNull Node x, int i) {
         Node z = new Node();
-        Node y = x.c[i];
+        Node y = x.c[i-1];
         z.leaf = y.leaf;
         z.n = t - 1;
         for (int j = 0; j < t - 1; j++)
             z.entries[j].key = y.entries[j + t].key;
         if (!y.leaf)
-            System.arraycopy(y.c, t, z.c, 0, t);
+            System.arraycopy(y.c, t, z.c, 0, t-1);
         y.n = t - 1;
-        System.arraycopy(x.c, i + 1, x.c, i + 2, x.n - i + 1);
-        x.c[i + 1] = z;
-        for (int j = x.n; j >= i; j--)
+        System.arraycopy(x.c, i, x.c, i + 1, x.n - i + 1);
+        x.c[i] = z;
+        for (int j = x.n-1; j >= i-1; j--)
             x.entries[j + 1].key = x.entries[j].key;
-        x.entries[i].key = y.entries[t].key;
+        x.entries[i-1].key = y.entries[t-1].key;
         x.n++;
     }
 
     private void BTreeInsertNonFull(@NotNull Node x, String k, String value) {
         int i = x.n;
         if (x.leaf) {
-            while (i >= 0 && k.compareTo(x.entries[i].key) < 0) {
-                x.entries[i + 1].key = x.entries[i].key;
-                x.entries[i + 1].setValue(x.entries[i].getValue());
+            while (i > 0 && k.compareTo(x.entries[i - 1].key) < 0) {
+                x.entries[i].key = x.entries[i - 1].key;
+                x.entries[i].setValue(x.entries[i - 1].getValue());
                 i--;
             }
-            x.entries[i + 1].key = k;
-            x.entries[i + 1].setValue(value);
+            x.entries[i].key = k;
+            x.entries[i].setValue(value);
             x.n++;
         } else {
-            while (i >= 0 && k.compareTo(x.entries[i].key) < 0)
+            while (i > 0 && k.compareTo(x.entries[i - 1].key) < 0)
                 i++;
             if (x.c[i].n == 2 * t - 1) {
                 BTreeSplitChild(x, i);
@@ -94,8 +94,13 @@ public class BPlusTreeWC extends StringTree {
     private class Node {
         boolean leaf;
         int n = 0;
-        BTreeEntry[] entries = new BTreeEntry[t];
-        Node[] c = new Node[t];
+        BTreeEntry[] entries = new BTreeEntry[2 * t - 1];
+        Node[] c = new Node[2 * t];
+
+        Node() {
+            for (int i = 0; i < 2 * t - 1; i++)
+                entries[i] = new BTreeEntry(null, null);
+        }
     }
 
     class BTreeEntry extends AbstractEntry {
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,16 @@ public class Main {
         redBlackTree.put("1", "一");
         redBlackTree.put("6", "一");
         redBlackTree.preOrderPrint();
+        BPlusTreeWC bPlusTreeWC = new BPlusTreeWC();
+        bPlusTreeWC.put("7", "一");
+        bPlusTreeWC.put("3", "二");
+        bPlusTreeWC.put("5", "一");
+        bPlusTreeWC.put("1", "三");
+        bPlusTreeWC.put("6", "一");
+        System.out.println(bPlusTreeWC.get("7"));
+        System.out.println(bPlusTreeWC.get("3"));
+        System.out.println(bPlusTreeWC.get("5"));
+        System.out.println(bPlusTreeWC.get("1"));
+        System.out.println(bPlusTreeWC.get("6"));
     }
 }
