9654d4dcef87c73a0f09a1a1dff7264fe1363185
StrayBird_ATSH <zhanghuilian-98022@163.com>
2018-10-30 20:37:37 +0800

    The current version of BTree is according to the book, and the delete method is not accomplished yet.

---
+++ src/BPlusTreeWC.java
@@ -0,0 +1,114 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+
+public class BPlusTreeWC extends StringTree<String, String> {
+    private Node root;
+    private int t = 2;
+
+    BPlusTreeWC() {
+        super();
+        root = new Node();
+        root.leaf = true;
+    }
+
+    @Override
+    public String get(Object key) {
+        return BTreeSearch(root, key.toString());
+    }
+
+    @Nullable
+    private String BTreeSearch(@NotNull Node x, String key) {
+        int i = 0;
+        while (i < x.n && key.compareTo(x.entries[i].getKey()) > 0)
+            i++;
+        if (i < x.n && key.equals(x.entries[i].getKey()))
+            return x.entries[i].getValue();
+        else if (x.leaf)
+            return null;
+        else return BTreeSearch(x.c[i], key);
+    }
+
+    @Override
+    public String put(String key, String value) {
+        String previousValue = get(key);
+        Node r = this.root;
+        if (root.n == 2 * t - 1) {
+            this.root = new Node();
+            root.leaf = false;
+            root.n = 0;
+            root.c[0] = r;
+            BTreeSplitChild(root, 1);
+            BTreeInsertNonFull(this.root, key, value);
+        } else BTreeInsertNonFull(r, key, value);
+        return previousValue;
+    }
+
+    private void BTreeSplitChild(@NotNull Node x, int i) {
+        Node z = new Node();
+        Node y = x.c[i];
+        z.leaf = y.leaf;
+        z.n = t - 1;
+        for (int j = 0; j < t - 1; j++)
+            z.entries[j].key = y.entries[j + t].key;
+        if (!y.leaf)
+            System.arraycopy(y.c, t, z.c, 0, t);
+        y.n = t - 1;
+        System.arraycopy(x.c, i + 1, x.c, i + 2, x.n - i + 1);
+        x.c[i + 1] = z;
+        for (int j = x.n; j >= i; j--)
+            x.entries[j + 1].key = x.entries[j].key;
+        x.entries[i].key = y.entries[t].key;
+        x.n++;
+    }
+
+    private void BTreeInsertNonFull(@NotNull Node x, String k, String value) {
+        int i = x.n;
+        if (x.leaf) {
+            while (i >= 0 && k.compareTo(x.entries[i].key) < 0) {
+                x.entries[i + 1].key = x.entries[i].key;
+                x.entries[i + 1].setValue(x.entries[i].getValue());
+                i--;
+            }
+            x.entries[i + 1].key = k;
+            x.entries[i + 1].setValue(value);
+            x.n++;
+        } else {
+            while (i >= 0 && k.compareTo(x.entries[i].key) < 0)
+                i++;
+            if (x.c[i].n == 2 * t - 1) {
+                BTreeSplitChild(x, i);
+                if (k.compareTo(x.entries[i].key) > 0)
+                    i++;
+            }
+            BTreeInsertNonFull(x.c[i], k, value);
+        }
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return null;
+    }
+
+    private class Node {
+        boolean leaf;
+        int n = 0;
+        BTreeEntry[] entries = new BTreeEntry[t];
+        Node[] c = new Node[t];
+    }
+
+    class BTreeEntry extends AbstractEntry<String, String> {
+        String key;
+
+        BTreeEntry(String key, String value) {
+            super(key, value);
+            this.key = key;
+        }
+
+        @Override
+        public String getKey() {
+            return this.key;
+        }
+    }
+}
